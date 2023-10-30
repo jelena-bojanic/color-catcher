ff33d911a88ff6df7f90d72529649bd72801aa29
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2023-10-30 19:26:04 +0100

    created function to extract monochormatic palette for a color

---
+++ src/utils/get-monochromatic-palette.ts
@@ -0,0 +1,35 @@
+import { Palette, colorRGB } from "../types";
+import { rgbToHex } from "./rgb-to-hex";
+import { rgbToHSL } from "./rgb-to-hsl";
+import { hslToRgb } from "./hsl-to-rgb";
+
+export function getMonochormaticPalette({
+  color,
+  count,
+}: {
+  color: colorRGB;
+  count: number;
+}): { palette: Palette } {
+  const shift = 33;
+
+  const monochormaticPalette = [{ rgb: color, hex: rgbToHex({ color }) }];
+  const colorHSL = rgbToHSL(color);
+
+  for (var i = 1; i < 3; i++) {
+    const monochormaticColor = {
+      ...colorHSL,
+    };
+    const lightnessStep = shift * i;
+    monochormaticColor.l = monochormaticColor.l + lightnessStep;
+    if (monochormaticColor.l > 100) {
+      monochormaticColor.l = monochormaticColor.l - 100;
+    }
+    const monochormaticRGB = hslToRgb(monochormaticColor);
+    monochormaticPalette.push({
+      rgb: monochormaticRGB,
+      hex: rgbToHex({ color: monochormaticRGB }),
+    });
+  }
+
+  return { palette: monochormaticPalette };
+}
