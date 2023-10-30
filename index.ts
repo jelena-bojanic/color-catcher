1cb208850c5acc5e7ad7e19c602e85136e9af785
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2023-10-30 19:27:08 +0100

    exposed all necessary functions and types

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { getComplementaryPalette } from "./src/utils/get-complementary-palette";
+import { getMonochormaticPalette } from "./src/utils/get-monochromatic-palette";
+import { getTriadicPalette } from "./src/utils/get-triadic-palette";
+import { getTetradicPalette } from "./src/utils/get-tetradic-palette";
+import { getImagePalette } from "./src/utils/get-image-palette";
+import { getMostFrequentColors } from "./src/utils/get-most-frequent-colors";
+
+import * as types from "./src/types";
+
+export { types };
+
+export {
+  getMostFrequentColors,
+  getComplementaryPalette,
+  getMonochormaticPalette,
+  getTriadicPalette,
+  getTetradicPalette,
+  getImagePalette,
+};
