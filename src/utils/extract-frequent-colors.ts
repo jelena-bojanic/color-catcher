06fc6bc68635683f8dd51ca9c55d783fd75a4909
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2023-11-08 17:05:51 +0100

    removing unused code and tiding up

--- src/utils/extract-frequent-colors.ts
+++ src/utils/extract-frequent-colors.ts
@@ -1,14 +1,5 @@
-import {
-  CanvasProperties,
-  ClassColor,
-  ColorCategories,
-  ColorClass,
-  ColorResult,
-  MostFrequentColor,
-  colorRGB,
-} from "../types";
+import { CanvasProperties, colorRGB } from "../types";
 import { createPixelArray } from "./create-pixel-array";
-import { findSimilarColor } from "./find-similar-color";
 import { rgbToHex } from "./rgb-to-hex";
 import { classifyColors } from "./classify-colors";
 import { compareColors } from "./comapre-colors";
@@ -25,49 +16,25 @@ export function extractFrequentColors({
   height,
 }: CanvasProperties) {
   const pixelData = canvasContext.getImageData(0, 0, width, height).data;
-  const pixelColors = createPixelArray({ pixels: pixelData });
 
-  const classes = classifyColors(pixelColors);
+  const { colorClasses: classes, countedColors } = classifyColors(pixelData);
 
-  console.log("before sorting", { classes });
-
-  const formatMostFreq = Object.values(classes)
+  const mostFrequentColors = Object.values(classes)
     .flatMap((colorClass) => {
-      return colorClass.colors;
+      return colorClass.colors.length ? colorClass.colors : [];
     })
     .map(({ color, count }) => {
       return {
         rgb: color,
         hex: rgbToHex({ color }),
         count: count,
-        precentage: ((count / pixelColors.length) * 100).toFixed(2),
+        precentage: ((count / countedColors) * 100).toFixed(2),
       };
     });
 
-  console.log({ formatMostFreq });
-
-  // const mostFreqFromEach = [];
-  // for (const c of Object.keys(classes)) {
-  //   if (!classes[c as ColorCategories].colors.length) {
-  //     continue;
-  //   }
-  //   const sortedColors = classes[c as ColorCategories].colors.sort(
-  //     (c1, c2) => c2.count - c1.count
-  //   );
-
-  //   const topColor1 = {
-  //     rgb: sortedColors[0].color,
-  //     hex: rgbToHex({ color: sortedColors[0].color }),
-  //     count: sortedColors[0].count,
-  //     precentage: ((sortedColors[0].count / pixelColors.length) * 100).toFixed(
-  //       2
-  //     ),
-  //   };
-
-  //   topColor1.count > 0 && mostFreqFromEach.push(topColor1);
-  // }
-
   return {
-    mostFrequentColors: formatMostFreq.sort((c1, c2) => c2.count - c1.count),
+    mostFrequentColors: mostFrequentColors.sort(
+      (c1, c2) => c2.count - c1.count
+    ),
   };
 }
