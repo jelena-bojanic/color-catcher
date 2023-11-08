3b3c0ef1e0f91037cb2851dd76f55842e4969396
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2023-11-08 17:03:39 +0100

    removed exporting not used function

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { getComplementaryPalette } from "./src/utils/get-complementary-palette";
 import { getMonochormaticPalette } from "./src/utils/get-monochromatic-palette";
 import { getTriadicPalette } from "./src/utils/get-triadic-palette";
 import { getTetradicPalette } from "./src/utils/get-tetradic-palette";
-import { getImagePalette } from "./src/utils/get-image-palette";
 import { getMostFrequentColors } from "./src/utils/get-most-frequent-colors";
 
 import * as types from "./src/types";
@@ -15,5 +14,4 @@ export {
   getMonochormaticPalette,
   getTriadicPalette,
   getTetradicPalette,
-  getImagePalette,
 };
