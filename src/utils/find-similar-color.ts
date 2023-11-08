4664e2d13b82c3b594272222b016d236c13e93e3
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2023-11-08 17:06:44 +0100

    removing check if color is the same, since its already done before function call and finding optimal value for delta e

--- src/utils/find-similar-color.ts
+++ src/utils/find-similar-color.ts
@@ -11,22 +11,13 @@ export function findSimilarColor({
   var bestMatch = { delta: Number.MAX_VALUE, index: 0 };
 
   for (var i = 0; i < colors.length; i = i + 1) {
-    // if (
-    //   color.r === colors[i].color.r &&
-    //   color.g === colors[i].color.g &&
-    //   color.b === colors[i].color.b
-    // ) {
-    //   bestMatch = { delta: 0, index: i };
-    //   break;
-    // }
     const delta = compareColors({ color1: colors[i].color, color2: color });
     if (delta < bestMatch.delta) {
       bestMatch = { delta: delta, index: i };
     }
   }
 
-  if (bestMatch.delta > 10) {
-    //blur faktor veza
+  if (bestMatch.delta > 17) {
     return null;
   }
 
